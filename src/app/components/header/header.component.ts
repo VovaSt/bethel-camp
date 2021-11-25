858b9ae916842c07be59d04452a438e691877cc4
Vova <volodymyr.stakhov@smartexe.com>
2021-11-25 19:04:14 +0200

    Develop

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    scrollTo(id: string) {
+        const element = document.getElementById(id);
+        element?.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    }
 }
--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
 import { LandingRoutingModule } from './landing-routing.module';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 
 
 @NgModule({
   declarations: [
-    LandingComponent
+    LandingComponent,
+    MainPageComponent
   ],
   imports: [
     CommonModule,
