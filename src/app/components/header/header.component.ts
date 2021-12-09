b80a9a7d54cab82584371182c3c476bcc0133b15
Vova <volodymyr.stakhov@smartexe.com>
2021-12-09 12:06:54 +0200

    Develop

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, Renderer2 } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements OnInit {
-    languages = ['EN', 'UA', 'RU'];
-    currentLang: string = 'UA';
 
-    constructor() {}
+    public languages = ['EN', 'UA', 'RU'];
+    public currentLang: string = 'UA';
 
-    ngOnInit() {}
+    private pagesLinks;
 
-    scrollTo(id: string) {
+    constructor(
+        private renderer: Renderer2
+    ) { }
+
+    ngOnInit() {
+        this.pagesLinks = document.querySelectorAll(".page-link");
+
+        fromEvent(window, 'scroll')
+            .pipe(debounceTime(50))
+            .subscribe(() => this.onScroll());
+    }
+
+    public scrollTo(id: string) {
         const element = document.getElementById(id);
         element?.scrollIntoView({
             behavior: 'smooth',
@@ -22,5 +36,23 @@ export class HeaderComponent implements OnInit {
         });
     }
 
-    changeLanguage(event) {}
+    private onScroll() {
+        const fromTop = window.scrollY;
+
+        this.pagesLinks.forEach(link => {
+            const sectionId = link.id?.split('_')[0];
+            const section = document.getElementById(sectionId);
+
+            if (
+                section.offsetTop <= fromTop &&
+                section.offsetTop + section.offsetHeight > fromTop
+            ) {
+                this.renderer.addClass(link, 'current');
+            } else {
+                this.renderer.removeClass(link, 'current');
+            }
+        });
+    }
+
+    changeLanguage(event) { }
 }
