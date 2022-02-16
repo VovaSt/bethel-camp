7e3999b0696e5f5ca5508abdde87f75b4d3b1606
VovaSt <vovastakhov@gmail.com>
2022-02-17 01:57:52 +0200

    temporary redirect to camp page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)
-  },
-  {
-    path: 'camp',
-    loadChildren: () => import('./modules/camp/camp.module').then(m => m.CampModule)
-  },
-  {
-    path: '**',
-    redirectTo: ''
-  }
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'camp' // remove after main pages developing
+        // loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)
+    },
+    {
+        path: 'camp',
+        loadChildren: () => import('./modules/camp/camp.module').then(m => m.CampModule)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
