d0adcd440a3989c61c91ab38e1cc42ff1e59681e
VovaSt <vovastakhov@gmail.com>
2022-03-03 15:59:12 +0200

    Develop confession page

--- src/app/modules/about/pages/confession/confession.component.ts
+++ src/app/modules/about/pages/confession/confession.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModulesManagerService } from 'src/app/core/services/module-manager.service';
+import { Topics } from './topics.map';
 
 @Component({
     selector: 'app-confession',
@@ -7,68 +8,7 @@ import { ModulesManagerService } from 'src/app/core/services/module-manager.serv
     styleUrls: ['./confession.component.scss'],
 })
 export class ConfessionComponent implements OnInit {
-    topics = {
-        bible: {
-            subject: 'confession.bible',
-            pCount: 10,
-        },
-        God: {
-            subject: 'confession.God',
-            pCount: 4,
-        },
-        GodFather: {
-            subject: 'confession.GodFather',
-            pCount: 12,
-        },
-        GodSon: {
-            subject: 'confession.GodSon',
-            pCount: 14,
-        },
-        HolySpirit: {
-            subject: 'confession.HolySpirit',
-            pCount: 5,
-        },
-        angels: {
-            subject: 'confession.angels',
-            pCount: 4,
-        },
-        devil: {
-            subject: 'confession.devil',
-            pCount: 8,
-        },
-        human: {
-            subject: 'confession.human',
-            pCount: 8,
-        },
-        lifeOfHuman: {
-            subject: 'confession.lifeOfHuman',
-            pCount: 6,
-        },
-        sin: {
-            subject: 'confession.sin',
-            pCount: 4,
-        },
-        salvation: {
-            subject: 'confession.salvation',
-            pCount: 4,
-        },
-        baptismWithHolySpirit: {
-            subject: 'confession.baptismWithHolySpirit',
-            pCount: 17,
-        },
-        giftsOfHolySpirit: {
-            subject: 'confession.giftsOfHolySpirit',
-            pCount: 7,
-        },
-        ChurchOfChrist: {
-            subject: 'confession.ChurchOfChrist',
-            pCount: 12,
-        },
-        localChurch: {
-            subject: 'confession.localChurch',
-            pCount: 9,
-        },
-    };
+    topics = Topics;
 
     topicKeys = Object.keys(this.topics);
 
---
+++ src/app/modules/about/pages/confession/topics.map.ts
@@ -0,0 +1,138 @@
+export const Topics = {
+    bible: {
+        subject: 'confession.bible',
+        pCount: 10,
+    },
+    God: {
+        subject: 'confession.God',
+        pCount: 4,
+    },
+    GodFather: {
+        subject: 'confession.GodFather',
+        pCount: 12,
+    },
+    GodSon: {
+        subject: 'confession.GodSon',
+        pCount: 14,
+    },
+    HolySpirit: {
+        subject: 'confession.HolySpirit',
+        pCount: 5,
+    },
+    angels: {
+        subject: 'confession.angels',
+        pCount: 4,
+    },
+    devil: {
+        subject: 'confession.devil',
+        pCount: 8,
+    },
+    human: {
+        subject: 'confession.human',
+        pCount: 8,
+    },
+    lifeOfHuman: {
+        subject: 'confession.lifeOfHuman',
+        pCount: 6,
+    },
+    sin: {
+        subject: 'confession.sin',
+        pCount: 4,
+    },
+    salvation: {
+        subject: 'confession.salvation',
+        pCount: 4,
+    },
+    baptismWithHolySpirit: {
+        subject: 'confession.baptismWithHolySpirit',
+        pCount: 17,
+    },
+    giftsOfHolySpirit: {
+        subject: 'confession.giftsOfHolySpirit',
+        pCount: 7,
+    },
+    ChurchOfChrist: {
+        subject: 'confession.ChurchOfChrist',
+        pCount: 12,
+    },
+    localChurch: {
+        subject: 'confession.localChurch',
+        pCount: 9,
+    },
+    homeChurch: {
+        subject: 'confession.homeChurch',
+        pCount: 6,
+    },
+    punishment: {
+        subject: 'confession.punishment',
+        pCount: 5,
+    },
+    ministers: {
+        subject: 'confession.ministers',
+        pCount: 6,
+    },
+    gathering: {
+        subject: 'confession.gathering',
+        pCount: 9,
+    },
+    prayer: {
+        subject: 'confession.prayer',
+        pCount: 10,
+    },
+    prayerForChild: {
+        subject: 'confession.prayerForChild',
+        pCount: 5,
+    },
+    prayerForSick: {
+        subject: 'confession.prayerForSick',
+        pCount: 5,
+    },
+    fasting: {
+        subject: 'confession.fasting',
+        pCount: 8,
+    },
+    confession: {
+        subject: 'confession.confession',
+        pCount: 7,
+    },
+    waterBaptism: {
+        subject: 'confession.waterBaptism',
+        pCount: 8,
+    },
+    Communion: {
+        subject: 'confession.Communion',
+        pCount: 8,
+    },
+    marriage: {
+        subject: 'confession.marriage',
+        pCount: 11,
+    },
+    otherBelievers: {
+        subject: 'confession.otherBelievers',
+        pCount: 5,
+    },
+    world: {
+        subject: 'confession.world',
+        pCount: 3,
+    },
+    power: {
+        subject: 'confession.power',
+        pCount: 3,
+    },
+    secondComing: {
+        subject: 'confession.secondComing',
+        pCount: 5,
+    },
+    millennium: {
+        subject: 'confession.millennium',
+        pCount: 4,
+    },
+    GreatJudgment: {
+        subject: 'confession.GreatJudgment',
+        pCount: 6,
+    },
+    newEarth: {
+        subject: 'confession.newEarth',
+        pCount: 5,
+    },
+};
