64b6f5563bebdc3ab010567b24a1fe8f57f04f65
VovaSt <vovastakhov@gmail.com>
2022-04-29 00:30:53 +0300

    Develop mobile layout of church-history page

--- src/app/shared/directives/image-loader.directive.ts
+++ src/app/shared/directives/image-loader.directive.ts
@@ -13,11 +13,18 @@ export class ImageLoaderDirective {
     }
   
     ngAfterViewInit() {
-        const img = new Image();
-        const bgStyle = getComputedStyle(this.el.nativeElement).backgroundImage;
-        const url = bgStyle.split('url')[1];
-        const src = url.replace('(','').replace(')','').replace(/\"/gi, "");
-        img.src = src;
+        let img;
+        
+        if (this.el.nativeElement.nodeName === 'IMG') {
+            img = this.el.nativeElement;
+        } else {
+            img = new Image();
+            const bgStyle = getComputedStyle(this.el.nativeElement).backgroundImage;
+            const url = bgStyle.split('url')[1];
+            const src = url.replace('(','').replace(')','').replace(/\"/gi, "");
+            img.src = src;
+        }
+
         img.addEventListener('load', () => this.loaderService.decreaseHardLoaderCount());
         img.addEventListener('error', () => this.loaderService.decreaseHardLoaderCount());
     }
